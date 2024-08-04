c010aa25c801818d802161d450d2ffd0859c1133
Andrew Grados <rewand.andrew@gmail.com>
2024-08-04 18:05:58 -0500

    ApiPostres-v1

---
+++ app/Models/Postres.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Postres extends Model
+{
+    use HasFactory;
+    protected $table = 'postres';
+    protected $fillable = ['nombre', 'precio', 'stock'];
+}
---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Resquest;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::resource('postres', 'Api\V1\PostresController');
+    Route::post('postres/{id}', 'Api\V1\PostresController@update');
+    Route::delete('postres/{id}', 'Api\V1\PostresController@destroy');
+});
